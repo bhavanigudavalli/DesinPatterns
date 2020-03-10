7b9ca9838accd47562651744b95622e168f532c4
bhavanigudavalli <39005965+bhavanigudavalli@users.noreply.github.com>
2020-03-10 23:49:15 +0530

    Add files via upload

---
+++ DesignPatterns/src/main/java/bhavani565/b565/App.java
@@ -0,0 +1,13 @@
+package bhavani565.b565;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
